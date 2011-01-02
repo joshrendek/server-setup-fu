0cd1e02cb601dd06f8dd0eb147ab6fb143a08337
Josh Rendek <joshrendek@sm2.h.bluescripts.net>
2011-01-01 22:15:03 -0500

    debian tweaks

--- debian-setup.sh
+++ debian-setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+# thanks to SpaceGhostC2C for several suggestions (irc.freenode)
 apt-get install curl
 curl https://github.com/bluescripts/server-setup-fu/raw/master/bash_profile > .bash_profile
 curl https://github.com/bluescripts/server-setup-fu/raw/master/bashrc > .bash_rc
@@ -6,7 +8,7 @@ apt-get install php5-cli
 apt-get install git git-core
 bash < <( curl http://rvm.beginrescueend.com/releases/rvm-install-head )
 echo '[[ -s "/usr/local/rvm/scripts/rvm" ]] && . "/usr/local/rvm/scripts/rvm"' >> .bash_profile
-source .bash_*
+source .bash_profile
 apt-get install bzip2
 apt-get install build-essential
 apt-get install zlib1g-dev
@@ -15,4 +17,5 @@ rvm package install openssl
 rvm install 1.9.2 --with-openssl-dir=/usr/local/rvm/usr
 rvm --default ruby-1.9.2
 gem install passenger --no-rdoc --no-ri
-
+gem install rails --no-rdoc --no-ri
+gem install bundler --no-rdoc --no-ri
