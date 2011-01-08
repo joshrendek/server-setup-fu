9ab689789aa439a2b13ea415f487bb2ea1e86c77
Josh Rendek <joshrendek@sm2.h.bluescripts.net>
2011-01-07 23:11:50 -0500

    assume yes for debian

--- debian-setup.sh
+++ debian-setup.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env sh
 # thanks to SpaceGhostC2C for several suggestions (github.com/SpaceGhost) including gemrc
-apt-get install curl
+apt-get install curl -y
 curl https://github.com/bluescripts/server-setup-fu/raw/master/bash_profile > .bash_profile
 curl https://github.com/bluescripts/server-setup-fu/raw/master/bashrc > .bash_rc
 curl https://github.com/bluescripts/server-setup-fu/raw/master/gemrc > .gemrc
-apt-get install php5
-apt-get install php5-cli
-apt-get install git git-core
+apt-get install php5 -y
+apt-get install php5-cli -y
+apt-get install git git-core -y
 bash < <( curl http://rvm.beginrescueend.com/releases/rvm-install-head )
 echo '[[ -s "/usr/local/rvm/scripts/rvm" ]] && . "/usr/local/rvm/scripts/rvm"' >> .bash_profile
 source .bash_profile
-apt-get install bzip2
-apt-get install build-essential
-apt-get install zlib1g-dev
-apt-get install libopenssl-ruby libcurl4-openssl-dev libssl-dev 
-rvm package install openssl
+apt-get install bzip2 -y
+apt-get install build-essential -y
+apt-get install zlib1g-dev -y
+apt-get install libopenssl-ruby libcurl4-openssl-dev libssl-dev -y
+rvm package install openssl -y
 rvm install 1.9.2 --with-openssl-dir=/usr/local/rvm/usr
 rvm --default ruby-1.9.2
 gem install passenger --no-rdoc --no-ri
